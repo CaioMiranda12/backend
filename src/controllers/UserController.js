3716d8d7dada64895bc270cea20667ec1464782e
CaioMiranda12 <caioclm04@gmail.com>
2024-09-17 19:10:24 -0300

    criando criptografia na senha durante a criação de usuario

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,19 +1,43 @@
 import { v4 } from 'uuid'
 import User from '../models/User'
+import * as Yup from 'yup'
 
 class UserController {
-    async store(req, res) {
-        const { name, email, password_hash, admin } = req.body
+    async store(request, response) {
+        const scheme = Yup.object({
+            name: Yup.string().required(),
+            email: Yup.string().email().required(),
+            password: Yup.string().min(6).required(),
+            admin: Yup.boolean()
+        })
+
+        try {
+            scheme.validateSync(request.body, {abortEarly: false})
+        } catch(err){
+            return response.status(400).json({error: err.errors})
+        }
+
+        const { name, email, password, admin } = request.body
+
+        const userExists = await User.findOne({
+            where: {
+                email,
+            }
+        })
+
+        if(userExists){
+            return response.status(400).json({error: 'User already exists'})
+        }
 
         const user = await User.create({
             id: v4(),
             name,
             email,
-            password_hash,
+            password,
             admin
         })
     
-        return res.status(201).json({
+        return response.status(201).json({
             id: user.id,
             name,
             email,
